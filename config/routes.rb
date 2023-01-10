5d204f8ab0804176686e8dabf8d084551d9d61bb
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2023-01-10 16:06:18 +0000

    3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,45 @@
 Rails.application.routes.draw do
-devise_for :customers,skip: [:passwords], controllers: {
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+    sessions: 'public/sessions'
+  }
 
-namespace :public do
-    get 'top' =>'homes#top'
-    get 'homes/about'
-
-    resources :items,only: [:index,:show]
-
-   	resources :customers,only: [:show,:edit,:update] do
-  		collection do
-  	     get 'quit'
-  	     patch 'out'
-  	  end
-  	end
-
-    resources :cart_items,only: [:index,:update,:create,:destroy] do
-      collection do
-        delete '/' => 'cart_items#all_destroy'
-      end
-    end
-
-    resources :orders,only: [:new,:index,:show,:create] do
-      collection do
-        post 'comfirm'
-        get 'thanx'
-      end
-    end
-
-    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
-  end
-
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
+  root to: "public/homes#top"
+  get 'about' => 'public/homes#about'
   namespace :admin do
-    get 'top'=>'homes#top'
-    resources :items,only: [:index,:new,:create,:show,:edit,:update,]
-    resources :genres,only: [:index,:create,:edit,:update]
-    resources :customers,only: [:index,:show,:edit,:update]
-    resources :order_items, only: [:update]
-    resources :orders,only: [:show,:update] do
-  	  member do
-        get :current_index
-        resource :order_details,only: [:update]
-      end
-      collection do
-        get :today_order_index
-      end
-    end
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :edit, :show, :update]
+    resources :orders, only: [:show, :update]
+    get '/' => 'homes#top'
   end
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+
+
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+
+    get 'orders/comfirm' => 'orders#comfirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :orders, only: [:new, :create, :index, :show]
+
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
